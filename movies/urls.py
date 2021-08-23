3bda5d5baa784e38dc4fc23ea139078a0ea790d9
n-varvonets <nickolay.varvonets@gmail.com>
2021-08-23 11:08:47 +0300

    worked detail_view, but without realated slug.. just with hardcoded url

--- movies/urls.py
+++ movies/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MoviesView.as_view())
+    path('', views.MoviesView.as_view()),
+    path('<int:pk>/', views.MovieDetailView.as_view()),
 ]
 
--- movies/views.py
+++ movies/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.base import View
 from .models import Movie
 
+
 class MoviesView(View):
     """List of movies"""
     def get(self, request):
@@ -9,5 +10,14 @@ class MoviesView(View):
         присланная от клиента(в данном случае от браузрера)"""
         movies = Movie.objects.all()
         return render(request, 'movies/movies.html', {'movie_list': movies})  # 3м параметром передаем...
+        #  контекст(, {'movie_list': movies})) - это словарь в котором есть ключи и его значения для темплейта
+
+
+class MovieDetailView(View):
+    """Full description of movie"""
+    def get(self, request, pk):  # pk - некое число, которое мы перендаем из url
+        movie = Movie.objects.get(id=pk)  # method get received ONLY one record (by pk)
+        print(movie.title, pk)
+        return render(request, 'movies/movie_detail.html', {'movie': movie})  # 3м параметром передаем...
         #  контекст(, {'movie_list': movies})) - это словарь в котором есть ключи и его значения для темплейта
 
