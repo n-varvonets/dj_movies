911798eb8a3a4643a181545d479e1974d5000e10
n-varvonets <nickolay.varvonets@gmail.com>
2021-08-25 15:03:44 +0300

    added rendering our img in dashboard

--- movies/admin.py
+++ movies/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import *
 
 
@@ -15,6 +17,19 @@ class ReviewInLine(admin.TabularInline):  # а вот TabularInline - гориз
     readonly_fields = ('name', 'email',)
 
 
+class MoviesShotsInline(admin.TabularInline):
+    """добавим возможность отображатьв нашей модели movie наши кадры (M2M / FK) -  поэтому создаем TabularInline"""
+    model = MovieShots
+    extra = 1
+    readonly_fields = ('get_image',)
+
+    def get_image(self, obj):
+        return mark_safe(
+            f'<img src={obj.image.url} width="150" height="auto"')
+
+    get_image.short_description = 'Изображение'
+
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = ('id',  'category', 'url', 'draft',)
@@ -24,7 +39,7 @@ class MovieAdmin(admin.ModelAdmin):
     search_fields = ('title', 'category__name', 'year')  # т.к. категория это обьект(ForeignKey), то нужно указать по какому полю обьекта проводить поиск (http://i.imgur.com/MCqQ49r.png)
     """что бы при отктие нашего фильма мы видели все отзывы прикрепленные к нему(т.к. отзывы это М2М/FK).
     Для этого создадим ниже класс ReviewInLine"""
-    inlines = [ReviewInLine]
+    inlines = [MoviesShotsInline, ReviewInLine]  # + добавим наш кадры к фильму
     """можно перенсти меню сохранение вверх"""
     save_on_top = True
     """можно настроить сохрнаить обьект как новый обьект, а старый останеться без изенений"""
@@ -32,12 +47,15 @@ class MovieAdmin(admin.ModelAdmin):
     """можно настроить реактирование параметра прямо из спика, для примера возьмум boolean value of draft"""
     list_editable = ('draft',) # до(http://i.imgur.com/iJMv07s.png) и после(http://i.imgur.com/5ZnkNE1.png)
     """можно поля M2M/FK  сделать в одну строку. проблема(http://i.imgur.com/oeYfrJR.png) + можно каким угодно способом гурпировать наши поля + давать именна.. т.е. вместо None  указать имя(http://i.imgur.com/lfKNvsZ.png)"""
+    readonly_fields = ('get_image', )
+
     fieldsets = (
         (None, {
             "fields": (("title", "tagline"),)
         }),
         (None, {
-            "fields": ("description", "poster")
+            # "fields": ("description", "poster")  - это рабочий вариант, но т.к. мы хотим добавить не строку, а картинку, то вызываем наш метод
+            "fields": ("description", ("poster",'get_image',))  # ("poster",'get_image',) - поместил в кортеж для того что бы в дашборде можно было добавитть постер и оно отображалось в одну строку
         }),
         (None, {
             "fields": (("year", "world_premiere", "country"),)
@@ -53,6 +71,14 @@ class MovieAdmin(admin.ModelAdmin):
             "fields": (("url", "draft"),)
         }),
     )  # http://i.imgur.com/y7XonNo.png
+
+    def get_image(self, obj):
+        """сделаем так что бы  постеры отображалось"""
+        return mark_safe(
+            f'<img src={obj.poster.url} width="50" height="auto"')  # и передаем урл нашего постера... для этого изменяем image на poster
+
+    # для того что бы изменить название колонки(http://i.imgur.com/jcu3cH9.png) нужен след метод
+    get_image.short_description = 'Poster'
 
 
 @admin.register(Reviews)
@@ -71,7 +97,14 @@ class GenreAdmin(admin.ModelAdmin):
 @admin.register(ActorOrDirector)
 class ActorAdmin(admin.ModelAdmin):
     """Актеры"""
-    list_display = ("name", "age")
+    list_display = ("name", "age", 'get_image')  # 4) и затем добавляем имя нашего метода
+    readonly_fields = ('get_image', )
+
+    """можно вывести картинку не как строку(http://i.imgur.com/SVxAqbi.png) а как изображение(http://i.imgur.com/X5eykUn.png) - 1) для этого пропишем def get_image(..)"""
+    def get_image(self, obj):  # obj - это обьект модели актеров
+        return mark_safe(f'<img src={obj.image.url} width="50" height="auto"')  # 2)mark_safe выведит html  не как строку, а как тэг
+
+    get_image.short_description = 'Изображение'  # 3)short_description добавляем описание - так будет называться наш столбец
 
 
 @admin.register(Rating)
@@ -83,9 +116,19 @@ class RatingAdmin(admin.ModelAdmin):
 @admin.register(MovieShots)
 class MovieShotsAdmin(admin.ModelAdmin):
     """Кадры из фильма"""
-    list_display = ("title", "movie")
+    list_display = ("title", "movie", 'get_image' )
+    readonly_fields = ('get_image',)
+
+    def get_image(self, obj):
+        return mark_safe(
+            f'<img src={obj.image.url} width="50" height="auto"')
+
+    get_image.short_description = 'Изображение'
 
 
+# можно изменить вывод титулки в дашборде http://i.imgur.com/MinGgPT.png
+admin.site.site_title = "Nick's movies"
+admin.site.site_header = "Nick's movies"
 
 # admin.site.register(Category, CategoryAdmin)  # для того что бы класс в админке работал его нужно зарегистровать.... но так же можно регистировать с помощью декоратора
 # admin.site.register(Genre)
